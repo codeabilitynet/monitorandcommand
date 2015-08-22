2651d540fc266f524f557154200ede6f7bf3e592
CodeAbility_cp <SND\CodeAbility_cp>
2015-08-22 07:05:35 +0000

    Added missing files

---
+++ CodeAbility.MonitorAndCommand/WpfMonitor/Converters/ConnectedToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CodeAbility.MonitorAndCommand.WpfMonitor.Converters
+{
+    public class ConnectedToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo language)
+        {
+            bool? connected = (bool?)value;
+
+            if (!connected.HasValue)
+                return "Connect"; 
+            else
+            {
+                return connected.Value ? "Connected" : "Error"; 
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo language)
+        {
+            //return null; 
+            throw new NotImplementedException();
+        }
+    }
+
+}
