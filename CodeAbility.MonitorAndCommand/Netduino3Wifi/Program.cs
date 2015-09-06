366111eec58bade5a8e085babf749b29ac9cf2bb
CodeAbility_cp <SND\CodeAbility_cp>
2015-09-06 18:27:14 +0000

    v.1.0.1

--- CodeAbility.MonitorAndCommand/Netduino3Wifi/Program.cs
+++ CodeAbility.MonitorAndCommand/Netduino3Wifi/Program.cs
@@ -29,7 +29,7 @@ namespace CodeAbility.MonitorAndCommand.Netduino3Wifi
 {
     public class Program
     {
-        const string IP_ADDRESS = "192.168.178.22";
+        const string IP_ADDRESS = "192.168.178.26";
         const int PORT = 11000;
 
         static System.Threading.AutoResetEvent _networkAvailableEvent = new System.Threading.AutoResetEvent(false);
