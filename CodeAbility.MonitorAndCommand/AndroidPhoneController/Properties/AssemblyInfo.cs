5ef25a5e6401f22bb5ab3bce51c571172056c5f5
CodeAbility_cp <SND\CodeAbility_cp>
2016-07-18 20:19:25 +0000

    Added AndroidPhoneController project

--- CodeAbility.MonitorAndCommand/AndroidPhoneController/Properties/AssemblyInfo.cs
+++ CodeAbility.MonitorAndCommand/AndroidPhoneController/Properties/AssemblyInfo.cs
@@ -1,18 +1,20 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeAbility.MonitorAndCommand.Netduino.Tools")]
+[assembly: AssemblyTitle("CodeAbility.MonitorAndCommand.AndroidPhoneController")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CodeAbility.MonitorAndCommand.Netduino.Tools")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("CodeAbility.MonitorAndCommand.AndroidPhoneController")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -21,5 +23,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
