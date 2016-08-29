0971d6382c01fcf8b2790abd6fed223d8ff9586a
CodeAbility_cp <SND\CodeAbility_cp>
2016-08-29 18:22:24 +0000

    Improved Android demo UI

--- CodeAbility.MonitorAndCommand/AndroidPhoneController/Resources/Resource.Designer.cs
+++ CodeAbility.MonitorAndCommand/AndroidPhoneController/Resources/Resource.Designer.cs
@@ -45,10 +45,19 @@ namespace CodeAbility.MonitorAndCommand.AndroidPhoneController
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int BlueLEDButton = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int LEDButton = 2130837505;
+			public const int GreenLEDButton = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int RedLEDButton = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int RGBLEDButton = 2130837508;
 			
 			static Drawable()
 			{
@@ -63,32 +72,137 @@ namespace CodeAbility.MonitorAndCommand.AndroidPhoneController
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int BoardLEDButton = 2131034114;
-			
-			// aapt resource value: 0x7f050004
-			public const int GreenLEDButton = 2131034116;
-			
-			// aapt resource value: 0x7f050003
-			public const int RedLEDButton = 2131034115;
+			// aapt resource value: 0x7f050000
+			public const int ConnectButton = 2131034112;
 			
 			// aapt resource value: 0x7f050001
-			public const int humidityTextView = 2131034113;
+			public const int Photon_A_LinearLayout = 2131034113;
+			
+			// aapt resource value: 0x7f05000b
+			public const int Photon_A_RGBLEDButton = 2131034123;
+			
+			// aapt resource value: 0x7f05000a
+			public const int Photon_A_RGBLEDLinearLayout = 2131034122;
 			
 			// aapt resource value: 0x7f050007
-			public const int seekBarBlue = 2131034119;
+			public const int Photon_A_boardLEDButton = 2131034119;
+			
+			// aapt resource value: 0x7f050004
+			public const int Photon_A_buttonsLinearLayout = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int Photon_A_deviceTextView = 2131034114;
+			
+			// aapt resource value: 0x7f050009
+			public const int Photon_A_greenLEDButton = 2131034121;
 			
 			// aapt resource value: 0x7f050006
-			public const int seekBarGreen = 2131034118;
+			public const int Photon_A_humidityTextView = 2131034118;
 			
-			// aapt resource value: 0x7f050005
-			public const int seekBarRed = 2131034117;
-			
-			// aapt resource value: 0x7f050000
-			public const int temperatureTextView = 2131034112;
+			// aapt resource value: 0x7f050003
+			public const int Photon_A_linearLayout = 2131034115;
 			
 			// aapt resource value: 0x7f050008
-			public const int toggleButton1 = 2131034120;
+			public const int Photon_A_redLEDButton = 2131034120;
+			
+			// aapt resource value: 0x7f05000f
+			public const int Photon_A_seekBarBlue = 2131034127;
+			
+			// aapt resource value: 0x7f05000e
+			public const int Photon_A_seekBarGreen = 2131034126;
+			
+			// aapt resource value: 0x7f05000d
+			public const int Photon_A_seekBarRed = 2131034125;
+			
+			// aapt resource value: 0x7f050005
+			public const int Photon_A_temperatureTextView = 2131034117;
+			
+			// aapt resource value: 0x7f050010
+			public const int Photon_B_LinearLayout = 2131034128;
+			
+			// aapt resource value: 0x7f05001a
+			public const int Photon_B_RGBLEDButton = 2131034138;
+			
+			// aapt resource value: 0x7f050019
+			public const int Photon_B_RGBLEDLinearLayout = 2131034137;
+			
+			// aapt resource value: 0x7f050016
+			public const int Photon_B_boardLEDButton = 2131034134;
+			
+			// aapt resource value: 0x7f050013
+			public const int Photon_B_buttonsLinearLayout = 2131034131;
+			
+			// aapt resource value: 0x7f050011
+			public const int Photon_B_deviceTextView = 2131034129;
+			
+			// aapt resource value: 0x7f050018
+			public const int Photon_B_greenLEDButton = 2131034136;
+			
+			// aapt resource value: 0x7f050015
+			public const int Photon_B_humidityTextView = 2131034133;
+			
+			// aapt resource value: 0x7f050012
+			public const int Photon_B_linearLayout = 2131034130;
+			
+			// aapt resource value: 0x7f050017
+			public const int Photon_B_redLEDButton = 2131034135;
+			
+			// aapt resource value: 0x7f05001d
+			public const int Photon_B_seekBarBlue = 2131034141;
+			
+			// aapt resource value: 0x7f05001c
+			public const int Photon_B_seekBarGreen = 2131034140;
+			
+			// aapt resource value: 0x7f05001b
+			public const int Photon_B_seekBarRed = 2131034139;
+			
+			// aapt resource value: 0x7f050014
+			public const int Photon_B_temperatureTextView = 2131034132;
+			
+			// aapt resource value: 0x7f05001e
+			public const int Photon_C_LinearLayout = 2131034142;
+			
+			// aapt resource value: 0x7f050028
+			public const int Photon_C_RGBLEDButton = 2131034152;
+			
+			// aapt resource value: 0x7f050027
+			public const int Photon_C_RGBLEDLinearLayout = 2131034151;
+			
+			// aapt resource value: 0x7f050024
+			public const int Photon_C_boardLEDButton = 2131034148;
+			
+			// aapt resource value: 0x7f050021
+			public const int Photon_C_buttonsLinearLayout = 2131034145;
+			
+			// aapt resource value: 0x7f05001f
+			public const int Photon_C_deviceTextView = 2131034143;
+			
+			// aapt resource value: 0x7f050026
+			public const int Photon_C_greenLEDButton = 2131034150;
+			
+			// aapt resource value: 0x7f050023
+			public const int Photon_C_humidityTextView = 2131034147;
+			
+			// aapt resource value: 0x7f050020
+			public const int Photon_C_linearLayout = 2131034144;
+			
+			// aapt resource value: 0x7f050025
+			public const int Photon_C_redLEDButton = 2131034149;
+			
+			// aapt resource value: 0x7f05002b
+			public const int Photon_C_seekBarBlue = 2131034155;
+			
+			// aapt resource value: 0x7f05002a
+			public const int Photon_C_seekBarGreen = 2131034154;
+			
+			// aapt resource value: 0x7f050029
+			public const int Photon_C_seekBarRed = 2131034153;
+			
+			// aapt resource value: 0x7f050022
+			public const int Photon_C_temperatureTextView = 2131034146;
+			
+			// aapt resource value: 0x7f05000c
+			public const int seekbarsLinearLayout = 2131034124;
 			
 			static Id()
 			{
